c19ad5b4c03597b801559a5d7ef1dcd4e3041e42
jokerkwu <pkjhj485@naver.com>
2021-06-23 19:17:46 +0900

    rest api 구축

--- main.go
+++ main.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	mux2 "github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+	"log"
 	"net/http"
 	"sort"
 	"strconv"
+	"time"
 )
 
 type Post struct{
-	Id int
-	Title string
-	Content string
-	Author string
+	Id int	`json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+	Author string `json:"author"`
+	Date string `json:"date"`
 }
-
-var posts map[int]Post	//게시물을 저장하는 변수
+type Success struct{
+	Success bool `json:"success"`
+}
+var posts map[int]Post	//게시물을 저장하는 맵형태 변수
 var lastId int
 type Posts []Post
-
+var rd *render.Render
 func MakeWebHandler() http.Handler{
 	mux := mux2.NewRouter()
+	mux.Handle("/",http.FileServer(http.Dir("public")))
 	//핸들러 등록 1. 전체 조회 , 2.조회, 3. 작성, 4. 삭제, 5. 수정
 	mux.HandleFunc("/posts",GetPostListHandler).Methods("GET")
 	mux.HandleFunc("/posts/{id:[0-9]+}",GetPostHandler).Methods("GET")
@@ -30,8 +37,8 @@ func MakeWebHandler() http.Handler{
 	mux.HandleFunc("/posts/{id:[0-9]+}",PutPostHandler).Methods("PUT")
 
 	posts = make(map[int]Post)
-	posts[1] = Post{1,"title1","content1","Ryan"}
-	posts[2] = Post{2,"title2","content2","Ryan"}
+	posts[1] = Post{1,"title1","content1","Ryan",time.Now().Format("2006-01-02 15:05:05")}
+	posts[2] = Post{2,"title2","content2","Ryan",time.Now().Format("2006-01-02 15:05:05")}
 
 	lastId = 2
 	return mux
@@ -48,27 +55,37 @@ func (p Posts) Less(i, j int) bool{
 }
 //게시물 수정
 func PutPostHandler(w http.ResponseWriter, r *http.Request){
-	vars := mux2.Vars(r)
-	id, _ := strconv.Atoi(vars["id"])
-	_, ok := posts[id]
-	if !ok{
-		w.WriteHeader(http.StatusNotFound)
+	var newPost Post
+	err := json.NewDecoder(r.Body).Decode(&newPost)
+	if err != nil{
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
+	vars := mux2.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if post, ok := posts[id]; ok{
+		post.Content = newPost.Content
+		post.Title = newPost.Title
+		post.Author = newPost.Author
+		posts[id] = post
+		rd.JSON(w, http.StatusOK, Success{true})
+	}else{
+		rd.JSON(w, http.StatusBadRequest,Success{false})
+	}
+
 }
 
 //게시물 제거
 func DeletePostHandler(w http.ResponseWriter, r *http.Request){
 	vars := mux2.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
-	_, ok := posts[id]
-	if !ok{
-		w.WriteHeader(http.StatusNotFound)
-		return
+	if _, ok := posts[id]; ok{
+		delete(posts,id)
+		rd.JSON(w,http.StatusOK,Success{true})
+	}else{
+		rd.JSON(w,http.StatusNotFound,Success{false})
 	}
-	delete(posts, id)
-	w.WriteHeader(http.StatusOK)
 }
 
 //게시물 추가
@@ -82,7 +99,7 @@ func PostPostHandler(w http.ResponseWriter,r *http.Request){
 	lastId++
 	post.Id = lastId
 	posts[lastId] = post
-	w.WriteHeader(http.StatusCreated)
+	rd.JSON(w,http.StatusCreated,post)
 }
 
 //게시물 조회
@@ -95,9 +112,7 @@ func GetPostHandler(w http.ResponseWriter, r *http.Request){
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(post)
+	rd.JSON(w, http.StatusOK, post)
 }
 
 // 게시물 리스트 조회
@@ -107,12 +122,18 @@ func GetPostListHandler(w http.ResponseWriter,r *http.Request){
 		list = append(list, post)
 	}
 	sort.Sort(list)
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(list)
+	rd.JSON(w, http.StatusOK, list)
 }
 
 func main(){
-	fmt.Println("서버 시작...")
-	http.ListenAndServe(":3000",MakeWebHandler())
+	rd = render.New()
+	m := MakeWebHandler()
+	n := negroni.Classic()
+	n.UseHandler(m)
+
+	log.Println("Server start...")
+	err := http.ListenAndServe(":8080",MakeWebHandler())
+	if err != nil{
+		panic(err)
+	}
 }
