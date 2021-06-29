a311dd609264a44b4b42835f28ecbc825f1de41b
jokerkwu <pkjhj485@naver.com>
2021-06-30 01:36:55 +0900

    sad

--- main.go
+++ main.go
@@ -15,17 +15,19 @@ import (
 	"strconv"
 	"strings"
 )
-type CustomValidator struct{
-		validator *validator.Validate
-	}
 
-type Success struct{
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+type Success struct {
 	Success bool `json:"success"`
 }
 
 type Template struct {
 	templates *template.Template
 }
+
 //GetTempFilesFromFolders is scans file path
 func GetTempFilesFromFolders(folders []string) []string {
 	var filepaths []string
@@ -40,6 +42,7 @@ func GetTempFilesFromFolders(folders []string) []string {
 	}
 	return filepaths
 }
+
 //Render is function to use template function
 /*
  template 기능을 사용하기 위한 함수
@@ -51,126 +54,127 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-
-func (cv *CustomValidator) Validate(i interface{}) error{
-	if err := cv.validator.Struct(i); err != nil{
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 	return nil
 }
+
 var rd *render.Render
 
-
 //게시물 수정
-func PutPostHandler(c echo.Context) (err error){
+func PutPostHandler(c echo.Context) (err error) {
 	post := new(mongodb.Post)
-	if err = c.Bind(post); err != nil{
-		c.Logger().Printf("PutPostHandler() - Bind Fail : " , post )
-		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	if err = c.Bind(post); err != nil {
+		c.Logger().Printf("PutPostHandler() - Bind Fail : ", post)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	if err = c.Validate(post); err != nil{
-		c.Logger().Printf("PutPostHandler() - Validate Fail : ",post)
-		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	if err = c.Validate(post); err != nil {
+		c.Logger().Printf("PutPostHandler() - Validate Fail : ", post)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	mdb, err:= mongodb.GetClient()
+	mdb, err := mongodb.GetClient()
 	defer mdb.Disconnect(context.Background())
-	if err != nil{
+	if err != nil {
 		return c.JSON(http.StatusInternalServerError, nil)
 	}
 	id, _ := strconv.Atoi(c.Param("id"))
-	postUpdated := mongodb.UpdatePost(mdb, post, bson.M{"id":id})
-	posts := mongodb.ReturnPostList(mdb,bson.M{})
+	postUpdated := mongodb.UpdatePost(mdb, post, bson.M{"id": id})
+	posts := mongodb.ReturnPostList(mdb, bson.M{})
 
-	if postUpdated > 0{
-		return c.Render(http.StatusOK,"index.html",posts)
-	}else{
-		return c.Render(http.StatusBadRequest,"index.html",posts)
+	if postUpdated > 0 {
+		return c.Render(http.StatusOK, "index.html", posts)
+	} else {
+		return c.Render(http.StatusBadRequest, "index.html", posts)
 	}
 }
 
 //게시물 제거
-func DeletePostHandler(c echo.Context) error{
-	id, _ :=strconv.Atoi(c.Param("id"))
+func DeletePostHandler(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
 	mdb, err := mongodb.GetClient()
 	defer mdb.Disconnect(context.Background())
-	if err != nil{
-		return c.Render(http.StatusInternalServerError,"error.html",Success{false})
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", Success{false})
 	}
-	posts := mongodb.ReturnPostList(mdb,bson.M{})
-	if postRemoved := mongodb.RemoveOnePost(mdb,bson.M{"id":id}); postRemoved > 0 {
-		return c.Render(http.StatusOK,"index.html",posts)
-	}else{
-		return c.Render(http.StatusNotFound,"error.html",Success{false})
+	posts := mongodb.ReturnPostList(mdb, bson.M{})
+	if postRemoved := mongodb.RemoveOnePost(mdb, bson.M{"id": id}); postRemoved > 0 {
+		return c.Render(http.StatusOK, "index.html", posts)
+	} else {
+		return c.Render(http.StatusNotFound, "error.html", Success{false})
 	}
 }
 
 //게시물 추가
 func PostPostHandler(c echo.Context) (err error) {
 	post := new(mongodb.Post)
-	if err = c.Bind(post); err != nil{
-		c.Logger().Printf("PostPostHandler() - Bind Fail : " , post )
-		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	if err = c.Bind(post); err != nil {
+		c.Logger().Printf("PostPostHandler() - Bind Fail : ", post)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	if err = c.Validate(post); err != nil{
-		c.Logger().Printf("PostPostHandler() - Validate Fail : ",post)
-		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	if err = c.Validate(post); err != nil {
+		c.Logger().Printf("PostPostHandler() - Validate Fail : ", post)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	p := mongodb.Post{post.Id,post.Title,post.Content,post.Author,post.Date}
+	p := mongodb.Post{post.Id, post.Title, post.Content, post.Author, post.Date}
 	mdb, err := mongodb.GetClient()
 	defer mdb.Disconnect(context.Background())
-	if err != nil{
-		return c.Render(http.StatusInternalServerError,"error.html",Success{false})
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", Success{false})
 	}
-	insertId := mongodb.InsertNewPost(mdb,p)
+	insertId := mongodb.InsertNewPost(mdb, p)
 	c.Logger().Print("post create complete!! : ", insertId)
-	posts := mongodb.ReturnPostList(mdb,bson.M{})
-	return c.Render(http.StatusOK,"index.html",posts)
-}
-//게시물 조회
-func GetPostHandler(c echo.Context) error{
-	mdb,err := mongodb.GetClient()
-	defer mdb.Disconnect(context.Background())
-	if err != nil{
-		return c.Render(http.StatusInternalServerError,"error.html" ,nil)
-	}
-	id,_ := strconv.Atoi(c.Param("id"))
-	if post := mongodb.ReturnPostOne(mdb, bson.M{"id":id}); post.Id == 0{
-		return c.Render(http.StatusBadRequest,"error.html" ,nil)
-	}else{
-		return c.Render(http.StatusOK,"post_content.html",[]mongodb.Post{post})
-	}
-}
-// 게시물 리스트 조회
-func GetPostListHandler(c echo.Context) error{
-	mdb,err := mongodb.GetClient()
-	defer mdb.Disconnect(context.Background())
-	if err != nil{
-		return c.Render(http.StatusInternalServerError,"error.html",nil)
-	}
-	posts := mongodb.ReturnPostList(mdb,bson.M{})
-	return c.Render(http.StatusOK,"index.html",posts)
+	posts := mongodb.ReturnPostList(mdb, bson.M{})
+	return c.Render(http.StatusOK, "index.html", posts)
 }
 
-func GetPostWriteHandler(c echo.Context) error{
+//게시물 조회
+func GetPostHandler(c echo.Context) error {
+	mdb, err := mongodb.GetClient()
+	defer mdb.Disconnect(context.Background())
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", nil)
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	if post := mongodb.ReturnPostOne(mdb, bson.M{"id": id}); post.Id == 0 {
+		return c.Render(http.StatusBadRequest, "error.html", nil)
+	} else {
+		return c.Render(http.StatusOK, "post_content.html", []mongodb.Post{post})
+	}
+}
+
+// 게시물 리스트 조회
+func GetPostListHandler(c echo.Context) error {
+	mdb, err := mongodb.GetClient()
+	defer mdb.Disconnect(context.Background())
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", nil)
+	}
+	posts := mongodb.ReturnPostList(mdb, bson.M{})
+	return c.Render(http.StatusOK, "index.html", posts)
+}
+
+func GetPostWriteHandler(c echo.Context) error {
 	//아이디 체크.
 	//...
-	return c.Render(http.StatusOK,"post_write.html",nil)
+	return c.Render(http.StatusOK, "post_write.html", nil)
 }
-func GetPostUpdateHandler(c echo.Context) error{
-	mdb,err := mongodb.GetClient()
+func GetPostUpdateHandler(c echo.Context) error {
+	mdb, err := mongodb.GetClient()
 	defer mdb.Disconnect(context.Background())
-	if err != nil{
-		return c.Render(http.StatusInternalServerError,"error.html" ,nil)
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", nil)
 	}
-	id,_ := strconv.Atoi(c.Param("id"))
-	if post := mongodb.ReturnPostOne(mdb, bson.M{"id":id}); post.Id == 0{
-		return c.Render(http.StatusBadRequest,"error.html" ,nil)
-	}else{
-		return c.Render(http.StatusOK,"post_write.html",[]mongodb.Post{post})
+	id, _ := strconv.Atoi(c.Param("id"))
+	if post := mongodb.ReturnPostOne(mdb, bson.M{"id": id}); post.Id == 0 {
+		return c.Render(http.StatusBadRequest, "error.html", nil)
+	} else {
+		return c.Render(http.StatusOK, "post_write.html", []mongodb.Post{post})
 	}
 }
 
-func main(){
+func main() {
 	dirs := []string{"./public/", "./public/static/include/"}
 	tempfiles := GetTempFilesFromFolders(dirs)
 	t := &Template{
@@ -183,11 +187,11 @@ func main(){
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Static("/static/","public")
+	e.Static("/static/", "public")
 	//CORS
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowMethods: []string{echo.GET,echo.HEAD,echo.PUT,echo.PATCH,echo.POST,echo.DELETE},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
 	}))
 	e.Static("/static/", "public")
 	e.Renderer = t
@@ -195,17 +199,17 @@ func main(){
 	/*
 		Handler Register
 		1. 전체 조회 2. 조회 3. 생성 4. 수정 5. 삭제
-	 */
-	e.GET("/",GetPostListHandler)
+	*/
+	e.GET("/", GetPostListHandler)
 	e.GET("/posts", GetPostListHandler)
-	e.GET("/posts/:id",GetPostHandler)
-	e.POST("/posts",PostPostHandler)
-	e.PUT("/posts/:id",PutPostHandler)
-	e.DELETE("/posts/:id",DeletePostHandler)
+	e.GET("/posts/:id", GetPostHandler)
+	e.POST("/posts", PostPostHandler)
+	e.PUT("/posts/:id", PutPostHandler)
+	e.DELETE("/posts/:id", DeletePostHandler)
 
 	//글작성 페이지 이동
-	e.GET("/posts/write",GetPostWriteHandler)
-	e.GET("/posts/write/:id",GetPostUpdateHandler)
+	e.GET("/posts/write", GetPostWriteHandler)
+	e.GET("/posts/write/:id", GetPostUpdateHandler)
 	// server start
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- mongodb/mongo.go
+++ mongodb/mongo.go
@@ -9,109 +9,114 @@ import (
 	"sort"
 	"time"
 )
-type Post struct{
-		Id int	`json:"id" bson:"id" validate:"required"`
-		Title string `json:"title" bson:"title" validate:"required"`
-		Content string `json:"content" bson:"content" validate:"required"`
-		Author string `json:"author" bson:"author" validate:"required"`
-		Date time.Time `json:"date" bson:"date" validate:"omitempty"'`
+
+type Post struct {
+	Id      int       `json:"id" bson:"id" validate:"required"`
+	Title   string    `json:"title" bson:"title" validate:"required"`
+	Content string    `json:"content" bson:"content" validate:"required"`
+	Author  string    `json:"author" bson:"author" validate:"required"`
+	Date    time.Time `json:"date" bson:"date" validate:"omitempty"'`
 }
-type PostID struct{
+type PostID struct {
 	Seq int `json:"seq" validate:"required"`
 }
 type Posts []Post
-func (p Posts) Len() int{
+
+func (p Posts) Len() int {
 	return len(p)
 }
-func (p Posts) Swap(i, j int){
+func (p Posts) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
-func (p Posts) Less(i, j int) bool{
+func (p Posts) Less(i, j int) bool {
 	return p[i].Id > p[j].Id
 }
 
-func GetClient() (*mongo.Client,error){
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+func GetClient() (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017/?connect=direct"))
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
-	return client,err
+	return client, err
 }
 
 //게시물 리스트 반환
 func ReturnPostList(client *mongo.Client, filter bson.M) []Post {
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	var posts Posts
 	collection := client.Database("webboard").Collection("posts")
 	cur, err := collection.Find(ctx, filter)
-	if err != nil{
+	if err != nil {
 		log.Println("Error on Finding all the documents", err)
 	}
-	for cur.Next(ctx){
+	for cur.Next(ctx) {
 		var post Post
 		err = cur.Decode(&post)
-		if err != nil{
-			log.Println("Error on Decoding the document",err)
+		if err != nil {
+			log.Println("Error on Decoding the document", err)
 		}
 		posts = append(posts, post)
 	}
 	sort.Sort(posts)
 	return posts
 }
+
 //특정 게시물 리턴
 func ReturnPostOne(client *mongo.Client, filter bson.M) Post {
-	ctx,cancel := context.WithTimeout(context.Background(),5 *time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	var post Post
 	collection := client.Database("webboard").Collection("posts")
-	documentReturned := collection.FindOne(ctx,filter)
+	documentReturned := collection.FindOne(ctx, filter)
 	documentReturned.Decode(&post)
 	return post
 }
+
 //게시물 생성
-func InsertNewPost(client *mongo.Client, post Post) interface{}{
-	ctx,cancel := context.WithTimeout(context.Background(),5 *time.Second)
+func InsertNewPost(client *mongo.Client, post Post) interface{} {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	collection := client.Database("webboard").Collection("posts")
 	post.Date = time.Now()
 
 	var postid PostID
 	collection2 := client.Database("webboard").Collection("counters")
-	res := collection2.FindOne(ctx,bson.M{})
+	res := collection2.FindOne(ctx, bson.M{})
 	res.Decode(&postid)
 	post.Id = postid.Seq + 1
-	collection2.UpdateOne(ctx,bson.M{"id":"postid"},bson.M{"$set":bson.M{"seq":postid.Seq+1}})
+	collection2.UpdateOne(ctx, bson.M{"id": "postid"}, bson.M{"$set": bson.M{"seq": postid.Seq + 1}})
 
-	insertResult, err := collection.InsertOne(ctx,post)
-	if err != nil{
+	insertResult, err := collection.InsertOne(ctx, post)
+	if err != nil {
 		log.Println("Error on inserting new post", err)
 	}
 	return insertResult.InsertedID
 }
 
 //게시물 삭제
-func RemoveOnePost(client *mongo.Client, filter bson.M) int64{
-	ctx,cancel := context.WithTimeout(context.Background(),5 *time.Second)
+func RemoveOnePost(client *mongo.Client, filter bson.M) int64 {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	collection := client.Database("webboard").Collection("posts")
-	deleteResult, err := collection.DeleteOne(ctx,filter)
-	if err != nil{
-		log.Println("Error on deleting one post",err)
+	deleteResult, err := collection.DeleteOne(ctx, filter)
+	if err != nil {
+		log.Println("Error on deleting one post", err)
 	}
 	return deleteResult.DeletedCount
 }
+
 //게시물 수정
-func UpdatePost(client *mongo.Client, updateData interface{}, filter bson.M) int64{
-	ctx,cancel := context.WithTimeout(context.Background(),5 *time.Second)
+func UpdatePost(client *mongo.Client, updateData interface{}, filter bson.M) int64 {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	collection := client.Database("webboard").Collection("posts")
-	updateQuery := bson.D{{Key:"$set",Value:updateData}}
-	updateResult, err := collection.UpdateOne(ctx,filter, updateQuery)
-	if err != nil{
-		log.Println("Error on updating one post",err)
+	updateQuery := bson.D{{Key: "$set", Value: updateData}}
+	updateResult, err := collection.UpdateOne(ctx, filter, updateQuery)
+	if err != nil {
+		log.Println("Error on updating one post", err)
 	}
 	return updateResult.ModifiedCount
 }
