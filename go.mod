df8772853a2ec3acc6113e4d2ff8c3dafe66cc09
jokerkwu <pkjhj485@naver.com>
2021-06-30 18:32:09 +0900

    핸들러 토큰 생성 완료

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module go_project
 go 1.16
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/go-playground/validator/v10 v10.6.1
 	github.com/labstack/echo v3.3.10+incompatible
@@ -11,6 +11,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/stretchr/testify v1.6.1
+	github.com/twinj/uuid v1.0.0 // indirect
 	github.com/unrolled/render v1.4.0
 	go.mongodb.org/mongo-driver v1.5.3
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -14,7 +16,15 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
+// jwtCustomClaims are custom claims extending default ones.
+type jwtCustomClaims struct {
+	Userid  string `json:"userid"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
 type CustomValidator struct{
 		validator *validator.Validate
 	}
@@ -40,17 +50,10 @@ func GetTempFilesFromFolders(folders []string) []string {
 	}
 	return filepaths
 }
-//Render is function to use template function
-/*
- template 기능을 사용하기 위한 함수
- w : http.status
- name : html 명
- data : 전달하고자 하는 데이터
-*/
+
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
-
 
 func (cv *CustomValidator) Validate(i interface{}) error{
 	if err := cv.validator.Struct(i); err != nil{
@@ -59,7 +62,6 @@ func (cv *CustomValidator) Validate(i interface{}) error{
 	return nil
 }
 var rd *render.Render
-
 
 //게시물 수정
 func PutPostHandler(c echo.Context) (err error){
@@ -126,6 +128,7 @@ func PostPostHandler(c echo.Context) (err error) {
 	posts := mongodb.ReturnPostList(mdb,bson.M{})
 	return c.Render(http.StatusOK,"index.html",posts)
 }
+
 //게시물 조회
 func GetPostHandler(c echo.Context) error{
 	c.Logger().Printf("GET으로 오나")
@@ -141,7 +144,8 @@ func GetPostHandler(c echo.Context) error{
 		return c.Render(http.StatusOK,"post_content.html",[]mongodb.Post{post})
 	}
 }
-// 게시물 리스트 조회
+
+//게시물 리스트 조회
 func GetPostListHandler(c echo.Context) error{
 	mdb,err := mongodb.GetClient()
 	defer mdb.Disconnect(context.Background())
@@ -152,11 +156,12 @@ func GetPostListHandler(c echo.Context) error{
 	return c.Render(http.StatusOK,"index.html",posts)
 }
 
+//게시글 작성 페이지 이동
 func GetPostWriteHandler(c echo.Context) error{
-	//아이디 체크.
-	//...
 	return c.Render(http.StatusOK,"post_write.html",nil)
 }
+
+//게시글 수정 페이지 이동
 func GetPostUpdateHandler(c echo.Context) error{
 	mdb,err := mongodb.GetClient()
 	defer mdb.Disconnect(context.Background())
@@ -170,18 +175,160 @@ func GetPostUpdateHandler(c echo.Context) error{
 		return c.Render(http.StatusOK,"post_update.html",[]mongodb.Post{post})
 	}
 }
+
+//로그인 페이지 이동
 func GetLoginPageHandler(c echo.Context) error{
 	return c.Render(http.StatusOK,"login.html",nil)
 }
+
+//회원가입 페이지 이동
 func GetJoinPageHandler(c echo.Context) error{
 	return c.Render(http.StatusOK,"join.html",nil)
 }
-func GetLoginHandler(c echo.Context) error{
-	//로그인 처리
-	return nil
+
+//로그인 핸들러
+func PostLoginHandler(c echo.Context) (err error) {
+	//1.요청으로부터 받은 사용자 정보를 디비 체킹한다.
+	user := new(mongodb.User)
+	if err = c.Bind(user); err != nil{
+		c.Logger().Printf("USER Bind Fail : " , user )
+		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	}
+	if err = c.Validate(user); err != nil{
+		c.Logger().Printf("User Validate Fail : ",user)
+		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	}
+	mdb,err := mongodb.GetClient()
+	defer mdb.Disconnect(context.Background())
+	if err != nil{
+		return c.Render(http.StatusInternalServerError,"error.html" ,nil)
+	}
+	dbUser := mongodb.ReturnUserOne(mdb,bson.M{"userid":user.Userid})
+	//디비 체킹한다.
+	if user.Userid != dbUser.Userid || user.Password != dbUser.Password {
+		return echo.ErrUnauthorized
+	}
+	//2.액세스,리프레시 토큰을 생성한다.
+	tokens, err := generateTokenPair()
+	if err != nil{
+		return err
+	}
+
+	// 토큰 쿠키에 저장
+	access_cookie := new(http.Cookie)
+	access_cookie.Name = "access_token"
+	access_cookie.Value = tokens["access_token"]
+	access_cookie.Expires = time.Now().Add(time.Hour * 1)
+
+	refresh_cookie := new(http.Cookie)
+	refresh_cookie.Name = "refresh_token"
+	refresh_cookie.Value = tokens["refresh_token"]
+	refresh_cookie.Expires = time.Now().Add(time.Hour * 24)
+
+	//3. 클라이언트에게 엑세스, 리프레쉬 토큰을 발급해준다.
+	c.SetCookie(access_cookie)
+	c.SetCookie(refresh_cookie)
+
+	posts := mongodb.ReturnPostList(mdb,bson.M{})
+	return c.Render(http.StatusOK,"index.html",posts)
 }
-func GetJoinHandler(c echo.Context) error{
+
+//리프레시 토큰 생성 핸들러
+func token(c echo.Context) error {
+	type tokenReqBody struct {
+		RefreshToken string `json:"refresh_token"`
+	}
+	tokenReq := tokenReqBody{}
+	c.Bind(&tokenReq)
+
+	// Parse takes the token string and a function for looking up the key.
+	// The latter is especially useful if you use multiple keys for your application.
+	// The standard is to use 'kid' in the head of the token to identify
+	// which key to use, but the parsed token (head and claims) is provided
+	// to the callback, providing flexibility.
+	token, err := jwt.Parse(tokenReq.RefreshToken, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("secret"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// Get the user record from database or
+		// run through your business logic to verify if the user can log in
+		if int(claims["sub"].(float64)) == 1 {
+
+			newTokenPair, err := generateTokenPair()
+			if err != nil {
+				return err
+			}
+
+			return c.JSON(http.StatusOK, newTokenPair)
+		}
+
+		return echo.ErrUnauthorized
+	}
+
+	return err
+}
+
+//토큰 생성 함수
+func generateTokenPair() (map[string]string, error){
+	//Create token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	//Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["sub"] = 1
+	claims["userid"] = "ryan"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil{
+		return nil, err
+	}
+
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	rtClaims := refreshToken.Claims.(jwt.MapClaims)
+	rtClaims["sub"] = 1
+	rtClaims["userid"] = "ryan"
+	rtClaims["exp"] = time.Now().Add(time.Hour * 24*30).Unix()
+	rtClaims["admin"] = true
+
+	rt, err := refreshToken.SignedString([]byte("secret"))
+	if err != nil{
+		return nil, err
+	}
+
+	return map[string]string{"access_token": t, "refresh_token":rt},nil
+}
+
+//회원가입 핸들러
+func PostJoinHandler(c echo.Context) (err error){
 	//회원가입 처리
+	user := new(mongodb.User)
+	if err = c.Bind(user); err != nil{
+		c.Logger().Printf("PostPostHandler() - Bind Fail : " , user )
+		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	}
+	if err = c.Validate(user); err != nil{
+		c.Logger().Printf("PostPostHandler() - Validate Fail : ",user)
+		return echo.NewHTTPError(http.StatusBadRequest,err.Error())
+	}
+	u := mongodb.User{user.Userid,user.Password}
+	mdb, err := mongodb.GetClient()
+	defer mdb.Disconnect(context.Background())
+	if err != nil{
+		return c.Render(http.StatusInternalServerError,"error.html",Success{false})
+	}
+	insertId := mongodb.InsertNewUser(mdb,u)
+	c.Logger().Print("User create complete!! : ", insertId)
+	return c.Render(http.StatusOK,"login.html",u)
+
 	return nil
 }
 
@@ -207,23 +354,30 @@ func main(){
 	e.Static("/static/", "public")
 	e.Renderer = t
 
-	/*
-		Handler Register
-		1. 전체 조회 2. 조회 3. 생성 4. 수정 5. 삭제
-	 */
-	e.GET("/",GetPostListHandler)
-	e.GET("/posts", GetPostListHandler)
+	//Custom JWT
+	e.POST("/token",token)
+	r := e.Group("/posts")
+	r.Use(middleware.JWTWithConfig(
+		middleware.JWTConfig{
+			SigningMethod: "HS256",
+			SigningKey: []byte("secret"),
+			TokenLookup: "cookie:access_token",
+		}))
+	//권한이 필요한 핸들러
 	e.GET("/posts/:id",GetPostHandler)
 	e.POST("/posts",PostPostHandler)
 	e.POST("/posts/:id",PutPostHandler)
 	e.DELETE("/posts/:id",DeletePostHandler)
-	// 로그인 및 회원가입
-	e.GET("/login",GetLoginHandler)
-	e.GET("/join",GetJoinHandler)
-
-	//글작성 페이지 이동
 	e.GET("/posts/write",GetPostWriteHandler)
 	e.GET("/posts/write/:id",GetPostUpdateHandler)
+
+	//권한이 필요하지 않는 핸들러
+	// 로그인 및 회원가입
+	e.GET("/", GetPostListHandler)
+	e.POST("/login",PostLoginHandler)
+	e.POST("/join",PostJoinHandler)
+
+	//글작성 페이지 이동
 	e.GET("/loginpage",GetLoginPageHandler)
 	e.GET("/joinpage",GetJoinPageHandler)
 	// server start
